d736eb372f7b95f496887f4465cacb41a50e7540
Gustav Kristensen <gustavkrist@gmail.com>
2024-09-29 00:18:37 +0200

    move nord colorscheme autocmd to plugin spec

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -128,29 +128,6 @@ local definitions = {
       end,
     },
   },
-  {
-    "Colorscheme",
-    {
-      pattern = { "nord" },
-      callback = function()
-        vim.cmd([[
-        hi @markup.heading.1.markdown guifg=#D08770
-        hi RenderMarkdownH1Bg guifg=#D08770 guibg=#3d3c44
-        hi @markup.heading.2.markdown guifg=#EBCB8B
-        hi RenderMarkdownH2Bg guifg=#EBCB8B guibg=#3f4247
-        hi @markup.heading.3.markdown guifg=#A3BE8C
-        hi RenderMarkdownH3Bg guifg=#A3BE8C guibg=#394147
-        hi @markup.heading.4.markdown guifg=#81A1C1
-        hi RenderMarkdownH4Bg guifg=#81A1C1 guibg=#363e4c
-        hi @markup.heading.5.markdown guifg=#B48EAD
-        hi RenderMarkdownH5Bg guifg=#B48EAD guibg=#3a3c4a
-        hi @markup.heading.6.markdown guifg=#D8DEE9
-        hi RenderMarkdownH6Bg guifg=#D8DEE9 guibg=#3d434f
-        hi! link NoiceLspProgressTitle @comment
-        ]])
-      end,
-    }
-  },
 }
 
 -- Taken from LunarVim
--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -232,6 +232,26 @@ return {
         opts.transparent = false
       end
       require("nord").setup(opts)
+      vim.api.nvim_create_autocmd("Colorscheme", {
+        pattern = { "nord" },
+        callback = function()
+          vim.cmd([[
+            hi @markup.heading.1.markdown guifg=#D08770
+            hi RenderMarkdownH1Bg guifg=#D08770 guibg=#3d3c44
+            hi @markup.heading.5.markdown guifg=#EBCB8B
+            hi RenderMarkdownH5Bg guifg=#EBCB8B guibg=#3f4247
+            hi @markup.heading.3.markdown guifg=#A3BE8C
+            hi RenderMarkdownH3Bg guifg=#A3BE8C guibg=#394147
+            hi @markup.heading.2.markdown guifg=#81A1C1
+            hi RenderMarkdownH2Bg guifg=#81A1C1 guibg=#363e4c
+            hi @markup.heading.4.markdown guifg=#B48EAD
+            hi RenderMarkdownH4Bg guifg=#B48EAD guibg=#3a3c4a
+            hi @markup.heading.6.markdown guifg=#D8DEE9
+            hi RenderMarkdownH6Bg guifg=#D8DEE9 guibg=#3d434f
+            hi! link NoiceLspProgressTitle @comment
+            ]])
+        end,
+      })
       vim.cmd("colorscheme nord")
     end,
     opts = {
